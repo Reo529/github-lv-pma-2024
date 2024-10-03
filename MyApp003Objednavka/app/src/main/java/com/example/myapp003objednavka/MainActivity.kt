bc5b51d755fc2e9c3dd613c72fa1d9b501431779
Lea Voldřichová <lea.voldrichova@tul.cz>
2024-10-03 15:25:19 +0200

    #app3 ma update

--- MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.kt
+++ MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.kt
@@ -1,25 +1,52 @@
 package com.example.myapp003objednavka
 
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp003objednavka.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         //enableEdgeToEdge()
 
-        setContentView(R.layout.activity_main)
 
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets*/
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-title = "Objednávka sedla"
+        setContentView(binding.root)
+
+
+        title = "Objednávka sedla"
+
+        binding.btnObjednavka.setOnClickListener {
+
+            //načtení ID vybraného radioButtonu z radioGroup
+            val rgSedla = binding.rgSedla.checkedRadioButtonId
+
+            val sedlo = findViewById<RadioButton>(rgSedla)
+
+
+            val kuze = binding.cbKuze.isChecked
+            val krem = binding.cbKremnakuzi.isChecked
+            val skluz = binding.cbProtiskluz.isChecked
+
+            val objednavkaText = "Souhrn objednávky: " +
+                    "${sedlo.text}" +
+                    (if(kuze) "; lepší kůže" else "") +
+                    (if(krem) "; krém na kůži" else "") +
+                    (if(skluz) "; protiskluzové mazání" else "")
+
+
+            binding.tvObjednavka.text = objednavkaText
+
+        }
 
         }
     }
