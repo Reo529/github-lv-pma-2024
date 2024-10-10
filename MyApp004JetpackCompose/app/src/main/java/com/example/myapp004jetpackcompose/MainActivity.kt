b0fd576e69e51625f4733ef535305961c652d669
Lea Voldřichová <lea.voldrichova@tul.cz>
2024-10-10 12:10:51 +0200

    #tady další commit

--- MyApp004JetpackCompose/app/src/main/java/com/example/myapp004jetpackcompose/MainActivity.kt
+++ MyApp004JetpackCompose/app/src/main/java/com/example/myapp004jetpackcompose/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapp004jetpackcompose.ui.theme.MyApp004JetpackComposeTheme
@@ -72,12 +73,13 @@ fun ComposePerson() {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Moje Aplikace", color = Color.White) }, // Nastaví barvu textu na bílou
+                title = { Text("Moje Aplikace", color = Color.White, textAlign = TextAlign.Center)
+                        }, // Nastaví barvu textu na bílou
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = Color.DarkGray,  // Nastaví pozadí na černé
                     //titleContentColor = Color.White // Nastaví barvu textu na bílou
                 )
-            )
+             )
         }
     ) {innerPadding ->
 
