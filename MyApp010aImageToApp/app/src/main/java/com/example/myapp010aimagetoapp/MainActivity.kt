2ce67becf90ef0c60b6e5618f553f932fbc737a2
Lea Voldřichová <lea.voldrichova@tul.cz>
2024-11-07 11:09:54 +0100

    #10 commit

---
+++ MyApp010aImageToApp/app/src/main/java/com/example/myapp010aimagetoapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapp010aimagetoapp
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapp010aimagetoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        uri: Uri? -> binding.ivImage.setImageURI(uri)
+        }
+    }
+}
