1667f927ee5061f8977caca92d05f198790fea00
Lea Voldřichová <lea.voldrichova@tul.cz>
2024-12-12 12:14:22 +0100

    #009 update2

--- MyApp009skolni/app/build.gradle.kts
+++ MyApp009skolni/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.myapp009skolni"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.myapp009skolni"
